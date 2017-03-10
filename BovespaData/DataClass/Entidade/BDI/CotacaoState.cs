7ef77e8c20ebfb93bc767a70249125ed5e677f38
Tebaldi <Tebaldi@DEEPWHITE>
2017-03-10 09:41:27 -0300

    Impoprt BDI bovespa (v1)

---
+++ BovespaData/DataClass/Entidade/BDI/CotacaoState.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BovespaData.DataClass.Entidade.BDI
+{
+    public class CotacaoState
+    {
+        //    01 -TIPREG -TIPO DE REGISTRO FIXO "02" N(02) 01 02
+        public int TipoDeRegistro { get; set; }
+
+        //    02 -CODBDI -CÓDIGO BDI UTILIZADO PARA CLASSIFICAR OS PAPÉIS NA EMISSÃO DO BOLETIM DIÁRIO DE INFORMAÇÕES VER TABELA ASSOCIADA NE001 X(02) 03 04
+        public string CodBdi { get; set; }
+
+        //    03 -DESBDI -DESCRIÇÃO DO CÓDIGO DE BDI X(30) 05 34
+        public string DescricaoBDI { get; set; }
+
+        //    04 -NOMRES -NOME RESUMIDO DA EMPRESA EMISSORA DO PAPEL X(12) 35 46
+        public string NomeResumido { get; set; }
+
+        //    05 -ESPECI -ESPECIFICAÇÃO DO PAPEL PARA PAPÉIS PERTENCENTES AO NOVO MERCADO, NAS POSIÇÕES 9 E 10 ESTÁ INDICADO: N1, N2 OU NM. X(10) 47 56
+        public string EspecificacaoPapel { get; set; }
+
+        //    06 -INDCAR -INDICADOR DE CARACTERÍSTICA DO PAPEL VER TABELA ASSOCIADA PA020 X(01) 57 57
+        public string IndicadorCaracteristica { get; set; }
+
+        //    07 -CODNEG -CÓDIGO DE NEGOCIAÇÃO X(12) 58 69
+        public string Ticker { get; set; }
+
+        //    08 -TPMERC -TIPO DE MERCADO CÓD. DO MERCADO EM QUE O PAPEL ESTÁ CADASTRADO VER TABELA ASSOCIADA PA003 N(03) 70 72
+        public int TipoDeMercado { get; set; }
+
+        //    09 – NOMERC – DESCRIÇÃO DO TIPO DE MERCADO X(15) 73 87
+        public string DescTipoMercado { get; set; }
+
+        //    10 – PRAZOT – PRAZO EM DIAS DO MERCADO A TERMO X(03) 88 90
+        public int Prazo { get; set; }
+
+        //    11 – PREABE – PREÇO DE ABERTURA DO PAPEL (PREÇO DO PRIMEIRO NEGÓCIO EFETUADO COM O PAPEL-MERCADO) N(09)V99 91 101
+        public decimal PrecoAbertura { get; set; }
+
+        //    12 – PREMAX – PREÇO MÁXIMO DO PAPELMERCADO NO PREGÃO N(09)V99 102 112
+        public decimal PrecoMaximo { get; set; }
+
+        //    13 – PREMIN – PREÇO MÍNIMO DO PAPELMERCADO NO PREGÃO N(09)V99 113 123
+        public decimal PrecoMinimo { get; set; }
+
+        //    14 – PREMED – PREÇO MÉDIO DO PAPELMERCADO NO PREGÃO N(09)V99 124 134
+        public decimal PrecoMedio { get; set; }
+
+        //    15 -PREULT -PREÇO DO ÚLTIMO NEGÓCIO EFETUADO COM O PAPEL-MERCADO N(09)V99 135 145
+        public decimal PrecoFechamento { get; set; }
+
+        //    16 -SINOSC -SINAL DA OSCILAÇÃO DO PREÇO DO PAPEL-MERCADO EM RELAÇÃO AO PREGÃO ANTERIOR VER TABELA ASSOCIADA PA019 X(01) 146 146
+        public string SinalOscilacao { get; set; }
+
+        //    17 -OSCILA -OSCILAÇÃO DO PREÇO DO PAPEL-MERCADO EM RELAÇÃO AO PREGÃO ANTERIOR N(03)V99 147 151
+        public decimal Oscilacao { get; set; }
+
+        //    18 -PREOFC -PREÇO DA MELHOR OFERTA DE COMPRA DO PAPEL-MERCADO N(09)V99 152 162
+        public decimal PrecoMelhorCompra { get; set; }
+
+        //    19 -PREOFV -PREÇO DA MELHOR OFERTA DE VENDA DO PAPEL-MERCADO N(09)V99 163 173
+        public decimal PrecoMelhorVenda { get; set; }
+
+        //    20 -TOTNEG -NEG. NÚMERO DE NEGÓCIOS EFETUADOS COM O PAPEL-MERCADO NO PREGÃO CORRENTE N(05) 174 178
+        public int TotalNegocios { get; set; }
+
+        //    21 -QUATOT -QUANTIDADE TOTAL DE TÍTULOS NEGOCIADOS NESTE PAPELMERCADO N(15) 179 193
+        public long Quantidade { get; set; }
+
+        //    22 -VOLTOT -VOLUME TOTAL DE TÍTULOS NEGOCIADOS NESTE PAPEL-MERCADO N(15)V99 194 210
+        public decimal Volume { get; set; }
+
+        //    23 -PREEXE -PREÇO DE EXERCÍCIO PARA O MERCADO DE OPÇÕES OU VALOR DO CONTRATO PARA O MERCADO DE TERMO SECUNDÁRIO N(09)V99 211 221
+        public decimal PrecoExercicio { get; set; }
+
+        //    24 -DATVEN -DATA DO VENCIMENTO PARA OS MERCADOS DE OPÇÕES, TERMO SECUNDÁRIO OU FUTURO FORMATO AAAAMMDD N(08) 222 229 
+        public DateTime DataVencimento { get; set; }
+
+        //    25 -INDOPC -INDICADOR DE CORREÇÃO DE PREÇOS DE EXERCÍCIOS OU VALORES DE CONTRATO PARA OS MERCADOS DE OPÇÕES, TERMO SECUNDÁRIO OU FUTURO, RESPECTIVAMENTE VER TABELA ASSOCIADA PA004 N(01) 230 230
+        public int IndicadorCorrecaoOpcao { get; set; }
+
+        //    26 -NOMIND -DESCRIÇÃO DO INDICADOR DE CORREÇÃO DE PREÇOS DE EXERCÍCIOS OU VALORES DE CONTRATO PARA OS MERCADOS DE OPÇÕES, TERMO SECUNDÁRIO OU FUTURO, RESPECTIVAMENTE X(15) 231 245
+        public string DescIndicadorCorrecao { get; set; }
+
+        //    27 -FATCOT -FATOR DE COTAÇÃO DO PAPEL VER TABELA ASSOCIADA EM021 N(07) 246 252
+        public int FatorCotacao { get; set; }
+
+        //    28 -PTOEXE -PREÇO DE EXERCÍCIO EM PONTOS PARA OPÇÕES REFERENCIADAS EM DÓLAR OU VALOR DE CONTRATO EM PONTOS PARA TERMO SECUNDÁRIO PARA OS REFERENCIADOS EM DOLAR, CADA PONTO EQUIVALE AO VALOR, NA MOEDA CORRENTE, DE UM CENTÉSIMO DA TAXA MÉDIA DO DÓLAR COMERCIAL. INTERBANCÁRIO DE FECHAMENTO DO DIA ANTERIOR, OU SEJA, 1 PONTO = 1/100 US$ N(07)V(06) 253 265
+        public decimal PrecoExercicioPontos { get; set; }
+
+        //    29 -CODISI -CÓDIGO DO PAPEL NO SISTEMA ISIN CÓDIGO DO PAPEL NO SISTEMA ISIN X(12) 266 277
+        public string CodISIN { get; set; }
+
+        //    30 -DISMES -NÚMERO DE DISTRIBUIÇÃO DO PAPEL NÚMERO DE SEQüÊNCIA DO PAPEL CORRESPONDENTE AO ESTADO DE DIREITO VIGENTE N(03) 278 280
+        public int DistribuicaoMES { get; set; }
+
+        //    31 -ESTILO – ESTILO ADOTADO PARA O EXERCÍCIO DE OPÇÕES DE COMPRA/VENDA VER TABELA ASSOCIADA PA015 N(01) 281 281 32 -NOMEST -DESCRIÇÃO DO ESTILO X(15) 282 296
+        public string EstiloOpc { get; set; }
+
+        //    33 -ICOATV -INDICADOR DE CORREÇÃO DE PREÇOS DE EXERCÍCIOS OU VALORES DE CONTRATO PARA OS MERCADOS DE OPÇÕES, TERMO SECUNDÁRIO OU FUTURO, RESPECTIVAMENTE VER TABELA ASSOCIADA PA004 N(03) 297 299
+        public int IndicadorCorrecaoOpcao2 { get; set; }
+
+        //    34 – OSCPRE -OSCILAÇÃO DO PREÇO DO PAPEL-MERCADO EM RELAÇÃO AO PREGÃO ANTERIOR N(05)V99 300 306
+        public decimal Oscilacao2 { get; set; }
+
+        //    35 -RESERVA EM BRANCO X(44) 307 350 
+        public string Reserva { get; set; }
+    }
+
+}
---
+++ BovespaData/DataClass/Entidade/BDI/IndiceState.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BovespaData.DataClass.Entidade.BDI
+{
+    public class IndiceState
+    {
+
+        //    01 - TIPREG - TIPO DE REGISTRO FIXO "01" N(02) 01 02
+        //    02 - IDENTI - IDENTIFICAÇÃO DO ÍNDICE VER TABELA ASSOCIADA IN001 N(02) 03 04
+        //    03 - NOMIND - NOME DO ÍNDICE X(30) 05 34
+        //    04 - IDCABE – ÍNDICE DE ABERTURA DO PREGÃO N(06) 35 40
+        //    05 - IDCMIN - ÍNDICE MÍNIMO DO PREGÃO N(06) 41 46
+        //    06 - IDCMAX - ÍNDICE MÁXIMO DO PREGÃO N(06) 47 52
+        //    07 - IDCMED - ÍNDICE DA MÉDIA ARITMÉTICA DOS ÍNDICES DO PREGÃO N(06) 53 58
+        //    08 - IDCLIQ - ÍNDICE PARA LIQUIDAÇÃO SOMENTE PARA O ÍNDICE "01" N(06) 59 64
+        //    09 – IDMAXA - ÍNDICE MÁXIMO DO ANO N(06) 65 70
+        //    10 - DATMAX - DATA DO ÍNDICE MÁXIMO DO ANO FORMATO AAAAMMDD N(08) 71 78
+        //    11 - IDMINA - ÍNDICE MÍNIMO DO ANO N(06) 79 84
+        //    12 - DATMIN - DATA ÍNDICE MÍNIMO DO ANO FORMATO AAAAMMDD N(08) 85 92
+        //    13 - IDCFEC - ÍNDICE DE FECHAMENTO N(06) 93 98
+        //    14 - SINEVO - SINAL DA EVOLUÇÃO PERCENTUAL DO ÍNDICE DE FECHAMENTO VER TABELA ASSOCIADA PA019 X(01) 99 99 
+        //    15 - EVOIND - EVOLUÇÃO PERCENTUAL DO ÍNDICE DE FECHAMENTO N(03)V99 100 104
+        //    16 - SINONT - SINAL DA EVOLUÇÃO PERCENTUAL DO ÍNDICE DE ONTEM VER TABELA ASSOCIADA PA019 X(01) 105 105
+        //    17 - EVONTE - EVOLUÇÃO PERCENTUAL DO ÍNDICE DE ONTEM N(03)V99 106 110
+        //    18 - SINSEM - SINAL DA EVOLUÇÃO PERCENTUAL DO ÍNDICE NA SEMANA VER TABELA ASSOCIADA PA019 X(01) 111 111
+        //    19 - EVOSEM - EVOLUÇÃO PERCENTUAL DO ÍNDICE NA SEMANA N(03)V99 112 116
+        //    20 - SI1SEM - SINAL DA EVOLUÇÃO PERCENTUAL DO ÍNDICE EM UMA SEMANA VER TABELA ASSOCIADA PA019 X(01) 117 117
+        //    21 - EV1SEM - EVOLUÇÃO PERCENTUAL DO ÍNDICE EM UMA SEMANA N(03)V99 118 122
+        //    22 - SINMES - SINAL DA EVOLUÇÃO PERCENTUAL DO ÍNDICE NO MÊS VER TABELA ASSOCIADA PA019 X(01) 123 123
+        //    23 - EVOMES - EVOLUÇÃO PERCENTUAL DO ÍNDICE NO MÊS N(03)V99 124 128
+        //    24 - SI1MES - SINAL DA EVOLUÇÃO PERCENTUAL DO ÍNDICE EM UM MÊS VER TABELA ASSOCIADA PA019 X(01) 129 129
+        //    25 - EV1MES - EVOLUÇÃO PERCENTUAL DO ÍNDICE EM UM MÊS N(03)V99 130 134
+        //    26 - SINANO - SINAL DA EVOLUÇÃO PERCENTUAL DO ÍNDICE NO ANO VER TABELA ASSOCIADA PA019 X(01) 135 135
+        //    27 - EVOANO - EVOLUÇÃO PERCENTUAL DO ÍNDICE NO ANO N(03)V99 136 140
+        //    28 - SI1ANO - SINAL DA EVOLUÇÃO PERCENTUAL DO ÍNDICE EM UM ANO VER TABELA ASSOCIADA PA019 X(01) 141 141
+        //    29 - EV1ANO - EVOLUÇÃO PERCENTUAL DO ÍNDICE EM UM ANO N(03)V99 142 146
+        //    30 - ACOALT - NÚMERO DE AÇÕES PERTENCENTES AO ÍNDICE QUE TIVERAM ALTA N(03) 147 149
+        //    31 - ACOBAI - NÚMERO DE AÇÕES PERTENCENTES AO ÍNDICE QUE TIVERAM BAIXA N(03) 150 152
+        //    32 - ACOEST - NÚMERO DE AÇÕES PERTENCENTES AO ÍNDICE QUE PERMANECERAM ESTÁVEIS N(03) 153 155
+        //    33 - ACOTOT - NÚMERO TOTAL DE AÇÕES PERTENCENTES AO ÍNDICE N(03) 156 158
+        //    34 - NNGIND - NÚMERO DE NEGÓCIOS COM AÇÕES PERTENCENTES AO ÍNDICE N(06) 159 164
+        //    35 - QTDIND - QUANTIDADE DE TÍTULOS NEGOCIADOS COM AÇÕES PERTENCENTES AO ÍNDICE N(15) 165 179
+        //    36 - VOLIND - VOLUME DOS NEGÓCIOS COM AÇÕES PERTENCENTES AO ÍNDICE N(15)V99 180 196
+        //    37 - IDCMDP - ÍNDICE DA MÉDIA PONDERADA N(06) 197 202
+        //    38 - RESERVA EM BRANCO X(148) 203 350 
+
+
+        #region "Properties Variables"
+
+        //    01 -TIPREG -TIPO DE REGISTRO FIXO "01" N(02) 01 02
+        public int TipoDeRegistro { get; set; }
+        //    02 -IDENTI -IDENTIFICAÇÃO DO ÍNDICE VER TABELA ASSOCIADA IN001 N(02) 03 04
+        public int IdentificaIndice { get; set; }
+        //    03 -NOMIND -NOME DO ÍNDICE X(30) 05 34
+        public string NomeIndice { get; set; }
+        //    04 -IDCABE – ÍNDICE DE ABERTURA DO PREGÃO N(06) 35 40
+        public decimal IndiceAbertura { get; set; }
+        //    05 -IDCMIN -ÍNDICE MÍNIMO DO PREGÃO N(06) 41 46
+        public decimal IndiceMinimo { get; set; }
+        //    06 -IDCMAX -ÍNDICE MÁXIMO DO PREGÃO N(06) 47 52
+        public decimal IndiceMaximo { get; set; }
+        //    07 -IDCMED -ÍNDICE DA MÉDIA ARITMÉTICA DOS ÍNDICES DO PREGÃO N(06) 53 58
+        public decimal IndiceMedia { get; set; }
+        //    08 -IDCLIQ -ÍNDICE PARA LIQUIDAÇÃO SOMENTE PARA O ÍNDICE "01" N(06) 59 64
+        public decimal IndideLiquidacao { get; set; }
+        //    09 – IDMAXA – ÍNDICE MÁXIMO DO ANO N(06) 65 70
+        public decimal AnoIndiceMaximo { get; set; }
+        //    10 -DATMAX -DATA DO ÍNDICE MÁXIMO DO ANO FORMATO AAAAMMDD N(08) 71 78
+        public DateTime AnoDataIndiceMax { get; set; }
+        //    11 -IDMINA -ÍNDICE MÍNIMO DO ANO N(06) 79 84
+        public decimal AnoIndiceMinimo { get; set; }
+        //    12 -DATMIN -DATA ÍNDICE MÍNIMO DO ANO FORMATO AAAAMMDD N(08) 85 92
+        public DateTime AnoDataIndiceMin { get; set; }
+        //    13 -IDCFEC -ÍNDICE DE FECHAMENTO N(06) 93 98
+        public decimal IndiceFechamento { get; set; }
+        //    14 -SINEVO -SINAL DA EVOLUÇÃO PERCENTUAL DO ÍNDICE DE FECHAMENTO VER TABELA ASSOCIADA PA019 X(01) 99 99 
+        public string SinalEvolucaoPercentual { get; set; }
+        //    15 -EVOIND -EVOLUÇÃO PERCENTUAL DO ÍNDICE DE FECHAMENTO N(03)V99 100 104
+        public decimal EvolucaoPercentualDeFechamento { get; set; }
+        //    16 -SINONT -SINAL DA EVOLUÇÃO PERCENTUAL DO ÍNDICE DE ONTEM VER TABELA ASSOCIADA PA019 X(01) 105 105
+        public string OntemSinalEvolucao { get; set; }
+        //    17 -EVONTE -EVOLUÇÃO PERCENTUAL DO ÍNDICE DE ONTEM N(03)V99 106 110
+        public decimal OntemEvolucaoPercentual { get; set; }
+        //    18 -SINSEM -SINAL DA EVOLUÇÃO PERCENTUAL DO ÍNDICE NA SEMANA VER TABELA ASSOCIADA PA019 X(01) 111 111
+        public string NaSemanaSinalEvolucao { get; set; }
+        //    19 -EVOSEM -EVOLUÇÃO PERCENTUAL DO ÍNDICE NA SEMANA N(03)V99 112 116
+        public decimal NaSemanaEvolucaoPercentual { get; set; }
+        //    20 -SI1SEM -SINAL DA EVOLUÇÃO PERCENTUAL DO ÍNDICE EM UMA SEMANA VER TABELA ASSOCIADA PA019 X(01) 117 117
+        public string EmUmaSemanaSinalEvolucao { get; set; }
+        //    21 -EV1SEM -EVOLUÇÃO PERCENTUAL DO ÍNDICE EM UMA SEMANA N(03)V99 118 122
+        public decimal EmUmaSemanaEvolucaoPercentual { get; set; }
+        //    22 -SINMES -SINAL DA EVOLUÇÃO PERCENTUAL DO ÍNDICE NO MÊS VER TABELA ASSOCIADA PA019 X(01) 123 123
+        public string NoMesSinalEvolucao { get; set; }
+        //    23 -EVOMES -EVOLUÇÃO PERCENTUAL DO ÍNDICE NO MÊS N(03)V99 124 128
+        public decimal NoMesEvolucaoPercentual { get; set; }
+        //    24 -SI1MES -SINAL DA EVOLUÇÃO PERCENTUAL DO ÍNDICE EM UM MÊS VER TABELA ASSOCIADA PA019 X(01) 129 129
+        public string EmUmMesSinalEvolucao { get; set; }
+        //    25 -EV1MES -EVOLUÇÃO PERCENTUAL DO ÍNDICE EM UM MÊS N(03)V99 130 134
+        public decimal EmUmMesEvolucaoPercentual { get; set; }
+        //    26 -SINANO -SINAL DA EVOLUÇÃO PERCENTUAL DO ÍNDICE NO ANO VER TABELA ASSOCIADA PA019 X(01) 135 135
+        public string NoAnoSinalEvolucao { get; set; }
+        //    27 -EVOANO -EVOLUÇÃO PERCENTUAL DO ÍNDICE NO ANO N(03)V99 136 140
+        public decimal NoAnoEvolucaoPercentual { get; set; }
+        //    28 -SI1ANO -SINAL DA EVOLUÇÃO PERCENTUAL DO ÍNDICE EM UM ANO VER TABELA ASSOCIADA PA019 X(01) 141 141
+        public string EmUmAnoSinalEvolucao { get; set; }
+        //    29 -EV1ANO -EVOLUÇÃO PERCENTUAL DO ÍNDICE EM UM ANO N(03)V99 142 146
+        public decimal EmUmAnoEvolucaoPercentual { get; set; }
+        //    30 -ACOALT -NÚMERO DE AÇÕES PERTENCENTES AO ÍNDICE QUE TIVERAM ALTA N(03) 147 149
+        public int AcoesNoIndiceEmAlta { get; set; }
+        //    31 -ACOBAI -NÚMERO DE AÇÕES PERTENCENTES AO ÍNDICE QUE TIVERAM BAIXA N(03) 150 152
+        public int AcoesNoIndiceEmBaixa { get; set; }
+        //    32 -ACOEST -NÚMERO DE AÇÕES PERTENCENTES AO ÍNDICE QUE PERMANECERAM ESTÁVEIS N(03) 153 155
+        public int AcoesNoIndiceEstaveis { get; set; }
+        //    33 -ACOTOT -NÚMERO TOTAL DE AÇÕES PERTENCENTES AO ÍNDICE N(03) 156 158
+        public int AcoesNoIndiceTotal { get; set; }
+        //    34 -NNGIND -NÚMERO DE NEGÓCIOS COM AÇÕES PERTENCENTES AO ÍNDICE N(06) 159 164
+        public int NegociosComAcoesDoIndice { get; set; }
+        //    35 -QTDIND -QUANTIDADE DE TÍTULOS NEGOCIADOS COM AÇÕES PERTENCENTES AO ÍNDICE N(15) 165 179
+        public Int64 QtdTitulosNegociadosIndice { get; set; }
+        //    36 -VOLIND -VOLUME DOS NEGÓCIOS COM AÇÕES PERTENCENTES AO ÍNDICE N(15)V99 180 196
+        public decimal VolumeNegociosDoIndice { get; set; }
+        //    37 -IDCMDP -ÍNDICE DA MÉDIA PONDERADA N(06) 197 202
+        public decimal IndiceMediaPonderada { get; set; }
+        //    38 -RESERVA EM BRANCO X(148) 203 350 
+        public string Reserva { get; set; }
+
+        #endregion
+    }
+}
